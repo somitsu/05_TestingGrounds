4004d55de4835105b91b9676e419eadf99ca66ee
DESKTOP-HHEAT17\SVO_member <shoji.omitsu@awtcus.com>
2017-10-18 21:33:08 -0700

    Initial commit

---
+++ Source/TestingGounds/TestingGoundsGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "TestingGoundsGameMode.h"
+#include "TestingGoundsHUD.h"
+#include "TestingGoundsCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATestingGoundsGameMode::ATestingGoundsGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ATestingGoundsHUD::StaticClass();
+}
