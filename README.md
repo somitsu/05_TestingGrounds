d0f516b419c289ccd433fedf330fe4192a41734a
DESKTOP-HHEAT17\SVO_member <shoji.omitsu@awtcus.com>
2017-11-07 10:55:28 -0800

    TG72 Merging Our TP and FP Files

--- README.md
+++ README.md
@@ -64,3 +64,4 @@ A Hunger-Games insipired FPS with large outdoor terrains.  Advanced AI, basic ne
 - TG69 Level Gating for Testing Grounds
 - TG70 Swapping Materials in Blueprint
 - TG71 Garbage Collecting Previous Tiles
+- TG72 Merging Our TP and FP Files
--- Source/TestingGounds/TestingGoundsGameMode.cpp
+++ Source/TestingGounds/TestingGoundsGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGoundsGameMode::ATestingGoundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
